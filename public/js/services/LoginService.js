12ac6c80538cba1527dcfae5eaf8263edd1c020d
phiph_57 <phiph_57@bitbucket.org>
2015-05-21 01:00:30 +0700

    ok services and factory

--- public/js/services/LoginService.js
+++ public/js/services/LoginService.js
@@ -2,14 +2,17 @@
  * Created by PHI on 5/19/2015.
  */
 angular.module('LoginService', []).factory('Login', ['$http','$location', function($http,$location) {
-    return function(data){
-        $http.post('/login',data)
+    return function(value){
+        $http.post('/login',value)
             .success(function(data){
-                if(type=='error'){
+                if(data.type=='error'){
 
                 }else{
                     $location.path('/user');
                 }
             })
+            .error(function(data){
+
+            });
     }
 }]);
