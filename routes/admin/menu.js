b6bb49651cf5023e6839934212e0e8209907ed75
phiph_57 <phiph_57@bitbucket.org>
2015-05-30 21:00:17 +0700

    past

--- routes/admin/menu.js
+++ routes/admin/menu.js
@@ -11,7 +11,7 @@ var admin_not_logged_in=require('./admin_not_logged_in');
 router.get('/',admin_not_logged_in,function(req,res,next){
     res.render('admin/menu',{admin:req.session.admin});
 });
-router.get('/list',admin_not_logged_in,function(req,res,next){
+router.get('/dish',admin_not_logged_in,function(req,res,next){
     connection.query('select * from mon_an',function(err,rows,fields){
         if(err) console.log(err);
         else if(isNull(rows)){
@@ -21,4 +21,14 @@ router.get('/list',admin_not_logged_in,function(req,res,next){
         }
     });
 });
+router.get('/list',admin_not_logged_in,function(req,res,next){
+    connection.query('select * from thuc_don_hang_ngay join mon_an on (mon_an.ma_mon=thuc_don_hang_ngay.ma_mon)',function(err,rows,fields){
+        if(err) console.log(err);
+        else if(isNull(rows)){
+            res.json({type:'error',content:''})
+        }else{
+            res.json({type:'success',content: rows});
+        }
+    });
+})
 module.exports = router;
