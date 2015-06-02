6de8203be7a49c21af815707206fc9d37e774f6e
phiph_57 <phiph_57@bitbucket.org>
2015-06-02 19:21:15 +0700

    update abort

--- routes/admin/track.js
+++ routes/admin/track.js
@@ -31,7 +31,6 @@ router.get('/register',admin_not_logged_in,function(req,res,next){
     }
 });
 router.get('/real',admin_not_logged_in,function(req,res,next){
-    console.log(req.query);
     if(req.query.month && req.query.year){
         sql='select day(ngay) as ngay,count(id) as soluong from thuc_te_an where month(ngay)='+req.query.month+' and year(ngay)='+req.query.year+' group by ngay';
         connection.query(sql,function(err,rows,fields){
@@ -39,7 +38,27 @@ router.get('/real',admin_not_logged_in,function(req,res,next){
                 console.log(err);
                 res.json({type:'error',content:'Đã có lỗi xảy ra!'});
             }else if(isNull(rows)){
-                res.json({type:'error',content:'Không có dữ liệu về đăng ký ăn!'})
+                res.json({type:'error',content:'Không có dữ liệu thực tế đến ăn!'})
+            }else{
+                res.json({type:'success',content:rows});
+            }
+        })
+    }
+    else{
+        res.json({type:'error',content:"Thiếu thông tin!"});
+    }
+});
+router.get('/abort',admin_not_logged_in,function(req,res,next){
+    if(req.query.month && req.query.year){
+
+        sql='select dang_ky_an.ngay as ngay, dang_ky_an.user_id as user_id, user.username as username, user.fullname as fullname, user.birthdate as birthdate, user.mobiphone as sdt from dang_ky_an left join thuc_te_an on (dang_ky_an.ngay=thuc_te_an.ngay and dang_ky_an.user_id=thuc_te_an.user_id) join user on(dang_ky_an.user_id=user.id) where thuc_te_an.id is null and month(dang_ky_an.ngay)='+req.query.month+' and year(dang_ky_an.ngay)='+req.query.year+' group by ngay';
+        console.log(sql);
+        connection.query(sql,function(err,rows,fields){
+            if(err) {
+                console.log(err);
+                res.json({type:'error',content:'Đã có lỗi xảy ra!'});
+            }else if(isNull(rows)){
+                res.json({type:'error',content:'Không có dữ liệu về việc không đến ăn!'})
             }else{
                 res.json({type:'success',content:rows});
             }
