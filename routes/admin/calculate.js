09dfb7713b12062e8888af0f91e33615197216f0
phiph_57 <phiph_57@bitbucket.org>
2015-06-05 00:46:15 +0700

    update calculate

--- routes/admin/calculate.js
+++ routes/admin/calculate.js
@@ -12,8 +12,9 @@ router.get('/',admin_not_logged_in,function(req,res,next){
     res.render('admin/calculate',{admin:req.session.admin});
 });
 router.get('/list',admin_not_logged_in,function(req,res){
-    connection.query('SELECT user.username as username,user.fullname as fullname, user.birthdate as birthdate, sum(mon_an.don_gia) as amount from dang_ky_an left join thuc_don_hang_ngay on dang_ky_an.ngay=thuc_don_hang_ngay.ngay left join mon_an on (thuc_don_hang_ngay.ma_mon=mon_an.ma_mon) left join user on user.id=dang_ky_an.user_id where ' +
-        'month(thuc_don_hang_ngay.ngay)='+ req.query.month+' and year(thuc_don_hang_ngay.ngay)='+req.query.year +' group by user.id;',function(err,rows,fields){
+    var sql='SELECT user.username as username,user.fullname as fullname, user.birthdate as birthdate,ISNULL(thanh_toan.ngay) as paid , sum(mon_an.don_gia) as amount from dang_ky_an left join thuc_don_hang_ngay on dang_ky_an.ngay=thuc_don_hang_ngay.ngay left join mon_an on (thuc_don_hang_ngay.ma_mon=mon_an.ma_mon) left join user on user.id=dang_ky_an.user_id left join thanh_toan on (thanh_toan.user_id=user.id and month(thanh_toan.ngay)=month(thuc_don_hang_ngay.ngay) and year(thanh_toan.ngay)=year(thuc_don_hang_ngay.ngay) ) ' +
+        'where month(thuc_don_hang_ngay.ngay)='+req.query.month+' and year(thuc_don_hang_ngay.ngay)='+req.query.year+' group by user.id;';
+    connection.query(sql,function(err,rows,fields){
         res.json({type:'success',content:rows});
     });
 });
